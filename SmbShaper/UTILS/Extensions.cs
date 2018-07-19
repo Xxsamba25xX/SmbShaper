c3c8d8a3972073fdc8ba1b8d49522a60831e00dc
Leonardo <leonardo.ramallo@juantoselli.com>
2018-07-18 22:20:13 -0300

    Terminado Normalize

--- SmbShaper/UTILS/Extensions.cs
+++ SmbShaper/UTILS/Extensions.cs
@@ -1,11 +1,44 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Text;
 
 namespace SmbShaper.UTILS
 {
-    public class Extensions
-    {
-		
-    }
+	public static class Extensions
+	{
+		public static Point Get(this Point me, Cardinals cardinal)
+		{
+			switch (cardinal)
+			{
+				case Cardinals.N:
+					return new Point(me.X, me.Y - 1);
+					break;
+				case Cardinals.NE:
+					return new Point(me.X + 1, me.Y - 1);
+					break;
+				case Cardinals.E:
+					return new Point(me.X + 1, me.Y);
+					break;
+				case Cardinals.SE:
+					return new Point(me.X + 1, me.Y + 1);
+					break;
+				case Cardinals.S:
+					return new Point(me.X, me.Y + 1);
+					break;
+				case Cardinals.SO:
+					return new Point(me.X - 1, me.Y + 1);
+					break;
+				case Cardinals.O:
+					return new Point(me.X - 1, me.Y);
+					break;
+				case Cardinals.NO:
+					return new Point(me.X - 1, me.Y - 1);
+					break;
+				default:
+					return me;
+					break;
+			}
+		}
+	}
 }
